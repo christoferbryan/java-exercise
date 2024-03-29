c5d3e1a1c9c8a233966e5b2839da1da94bfcbcb0
Christofer Bryan <christoferbryan45@gmail.com>
2024-03-30 02:02:23 +0800

    feat(todolist): started adding Authentication class

--- src/main/java/com/adepuu/exercises/session8/Auth.java
+++ src/main/java/com/adepuu/exercises/session8/Auth.java
@@ -3,5 +3,8 @@ package com.adepuu.exercises.session8;
 import java.util.HashMap;
 
 public class Auth {
+    private HashMap<String, User> registeredUsers = new HashMap<>();
+    private String activeUserName;
+
 
 }
